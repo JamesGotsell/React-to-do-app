4f904892a13309f8edb4f092f48e439d5ecdc612
AndyLegacy <andystav91@googlemail.com>
2018-01-21 23:02:02 +0000

    adding component logic

--- todolist/src/TodoList/TodoList.js
+++ todolist/src/TodoList/TodoList.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
+import TodoItems from './TodoItems/TodoItems';
 
 
 class TodoList extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      items: []
+    };
+
+    this.addItem = this.addItem.bind(this);
+
+  }
+
+  addItem(e) {
+    const itemArray = this.state.items;
+
+    if (this._inputElement.value !== '') {
+      itemArray.unshift({
+        text: this._inputElement.value,
+        key: Date.now()
+      });
+
+      this._inputElement.value = '';
+    }
+    console.log(itemArray);
+
+    e.preventDefault();
+  }
+
   render() {
 
     return (
@@ -9,15 +37,16 @@ class TodoList extends Component {
 
         <div className='header'>
 
-          <form>
-
-            <input placeholder='enter task'></input>
+          <form onSubmit={this.addItem}>
+            <input ref={(a) => this._inputElement = a}
+              placeholder='enter task'></input>
 
             <button type='submit'>add</button>
 
           </form>
 
         </div>
+        <TodoItems entries={this.state.items}/>
 
       </div>
 
